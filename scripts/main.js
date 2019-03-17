d8a84c23753ec0f4792af18cfe1ff1c04ae8ab45
Kaipto <evgeniak885@gmail.com>
2019-03-17 18:07:53 +0200

    Markup

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,28 @@
-// scripts
+$(document).ready(function(){
+	$('a.menu-link.hover-menu').click(function(event) {
+        event.preventDefault();
+        $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top - 100 }, 700);
+        if(window.innerWidth <= 768 ) {
+        	$('.menu').slideUp(200);
+        }
+        
+    });
+	$('.burger img').click(function(){
+		if ($('.menu').is(':hidden')) {
+			$('.menu').slideDown(300);
+			$('header').addClass('bx-shadow');
+		} else {
+			$('.menu').slideUp(200);
+			$('header').removeClass('bx-shadow');
+		}
+		
+	});
+});
+
+$(window).scroll(function(){
+	if ($(this).scrollTop() > 0) {
+		$('header').addClass('sticky');
+	} else {
+		$('header').removeClass('sticky bx-shadow');
+	}
+});
